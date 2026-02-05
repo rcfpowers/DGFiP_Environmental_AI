d28f62422ce02d4c063f9a3b0077c387fc1c020e
rcfpowers <r.campbell.powers@gmail.com>
2026-02-05 20:59:44 +0000

    Added details on huggingface information and task for each model used

--- data_exploration.py
+++ data_exploration.py
@@ -12,4 +12,38 @@ FILE_PATH_S3 = BUCKET + "/" + FILE_KEY_S3
 with fs.open(FILE_PATH_S3, mode="rb") as file_in:
     df = pd.read_csv(file_in, sep=",")
 
-print(df.shape)
+
+
+df.loc[df['Model'] == "Llama-3-3-70B-128k", 'Task'] = 'Text_Generation'
+URL = 'https://huggingface.co/meta-llama/Meta-Llama-3-70B'
+df.loc[df['Model'] == 'Llama-3-3-70B-128k', 'HF_URL'] = URL
+
+df.loc[df['Model'] == "gptoss120b", 'Task'] = 'Reasoning'
+URL = 'https://huggingface.co/openai/gpt-oss-120b'
+df.loc[df['Model'] == 'gptoss120b', 'HF_URL'] = URL
+
+df.loc[df['Model'] == "gptoss20b", 'Task'] = 'Reasoning'
+URL = "https://huggingface.co/openai/gpt-oss-20b"
+df.loc[df['Model'] == 'gptoss20b', 'HF_URL'] = URL
+
+df.loc[df['Model'] == '/model/deepdml-faster-whisper-large-v3-turbo-ct2', 'Task'] = 'Automatic_Speech_Recognition'
+URL = 'https://huggingface.co/deepdml/faster-whisper-large-v3-turbo-ct2'
+df.loc[df['Model'] == '/model/deepdml-faster-whisper-large-v3-turbo-ct2', 'HF_URL'] = URL
+
+df.loc[df['Model'] == 'Qwen2.5-Coder-32B-Instruct-fp8-W8A16', 'Task'] = 'Text_Generation'
+URL = 'https://huggingface.co/Qwen/Qwen2.5-Coder-32B-Instruct'
+df.loc[df['Model'] == 'Qwen2.5-Coder-32B-Instruct-fp8-W8A16', 'HF_URL'] = URL
+
+df.loc[df['Model'] == 'gte-Qwen2-1-5B-instruct', 'Task'] = 'Sentence_Similarity'
+URL = 'https://huggingface.co/Alibaba-NLP/gte-Qwen2-1.5B-instruct'
+df.loc[df['Model'] == 'gte-Qwen2-1-5B-instruct', 'HF_URL'] = URL
+
+df.loc[df['Model'] == 'Mistral-Small-24B-Instruct-2501-FP8-dynamic', 'Task'] = 'Reasoning'
+URL = 'https://huggingface.co/mistralai/Mistral-Small-24B-Instruct-2501'
+df.loc[df['Model'] == 'Mistral-Small-24B-Instruct-2501-FP8-dynamic', 'HF_URL'] = URL
+
+df.loc[df['Model'] == 'dgfip-e5-large', 'Task'] = 'Sentence_Similarity'
+URL = 'https://huggingface.co/intfloat/e5-large'
+df.loc[df['Model'] == 'dgfip-e5-large', 'HF_URL'] = URL
+
+print(df['HF_URL'].unique())
